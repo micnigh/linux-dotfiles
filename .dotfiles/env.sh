60de6a73b092086bec0bbc60a0c654f58a332eb1
Michael Nigh <mic.nigh@gmail.com>
2015-11-15 11:45:52 -0800

    Cleanup git branch prompt

--- .dotfiles/env.sh
+++ .dotfiles/env.sh
@@ -4,5 +4,5 @@ export PATH=$PATH:$GOPATH/bin
 export PATH="$PATH:$HOME/npm/bin"
 export PATH="$PATH:$HOME/npm"
 
-# include git branch - see http://stackoverflow.com/a/16252145
-PS1=$PS1'$(__git_ps1 "\[\e[0;33m\](%s) \[\e[0m\]")'
+# include git branch - see http://stackoverflow.com/a/16252145 and http://unix.stackexchange.com/a/144330
+PS1=${PS1::-3}'$(__git_ps1 "\[\e[0;33m\](%s)\[\e[0m\]")\$ '
