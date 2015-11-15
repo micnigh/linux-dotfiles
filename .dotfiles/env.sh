bff80dd46d0203c7ee60764bb7d67d62464b30f2
Michael Nigh <mic.nigh@gmail.com>
2015-11-15 11:24:35 -0800

    Add git branch

--- .dotfiles/env.sh
+++ .dotfiles/env.sh
@@ -3,3 +3,6 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH="$PATH:$HOME/npm/bin"
 export PATH="$PATH:$HOME/npm"
+
+# include git branch - see http://stackoverflow.com/a/16252145
+PS1=$PS1'$(__git_ps1 "\[\e[0;33m\](%s) \[\e[0m\]")'
